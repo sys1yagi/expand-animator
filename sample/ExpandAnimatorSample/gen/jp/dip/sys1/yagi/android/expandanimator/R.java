a685f0cdaac257ea16a93f604705c9e8543fa2d8
yagi <sylc.yagi@gmail.com>
2012-02-10 19:52:51 +0900

    メニューのレイアウトを分けた。

--- sample/ExpandAnimatorSample/gen/jp/dip/sys1/yagi/android/expandanimator/R.java
+++ sample/ExpandAnimatorSample/gen/jp/dip/sys1/yagi/android/expandanimator/R.java
@@ -11,21 +11,37 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int android1=0x7f020000;
+        public static final int android2=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
+        public static final int add=0x7f050004;
         public static final int container1=0x7f050001;
         public static final int container2=0x7f050003;
-        public static final int container3=0x7f050005;
+        public static final int container2_1=0x7f050006;
+        public static final int container3=0x7f050008;
+        public static final int container4=0x7f05000a;
+        public static final int container5=0x7f05000c;
+        public static final int remove=0x7f050005;
         public static final int trigger1=0x7f050000;
         public static final int trigger2=0x7f050002;
-        public static final int trigger3=0x7f050004;
+        public static final int trigger3=0x7f050007;
+        public static final int trigger4=0x7f050009;
+        public static final int trigger5=0x7f05000b;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int menu_01=0x7f030001;
+        public static final int menu_02=0x7f030002;
+        public static final int menu_03=0x7f030003;
+        public static final int menu_04=0x7f030004;
+        public static final int menu_05=0x7f030005;
     }
     public static final class string {
+        public static final int add=0x7f040002;
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int remove=0x7f040003;
+        public static final int title=0x7f040000;
     }
 }
--- sample/ExpandAnimatorSample/src/jp/dip/sys1/yagi/android/expandanimator/sample/ExpandAnimatorSampleActivity.java
+++ sample/ExpandAnimatorSample/src/jp/dip/sys1/yagi/android/expandanimator/sample/ExpandAnimatorSampleActivity.java
@@ -16,13 +16,20 @@
  */
 package jp.dip.sys1.yagi.android.expandanimator.sample;
 
+import java.util.Random;
+
 import jp.dip.sys1.yagi.android.expandanimator.ExpandAnimator;
 import jp.dip.sys1.yagi.android.expandanimator.ExpandAnimatorManager;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * ExpandAnimator、ExpandAnimatorManagerを利用するサンプルプログラムです。
@@ -31,83 +38,162 @@ import android.view.animation.AccelerateDecelerateInterpolator;
  * 
  */
 public class ExpandAnimatorSampleActivity extends Activity {
-	private final static String TAG = ExpandAnimatorSampleActivity.class.getSimpleName();
+    private final static String TAG = ExpandAnimatorSampleActivity.class.getSimpleName();
+    private ExpandAnimatorSampleActivity This(){
+        return this;
+    }
+    private final static String CONTAINER_1 = "1";
+    private final static String CONTAINER_2 = "2";
+    private final static String CONTAINER_3 = "3";
+    private final static String CONTAINER_4 = "4";
+    private final static String CONTAINER_5 = "5";
 
-	private final static String CONTAINER_1 = "1";
-	private final static String CONTAINER_2 = "2";
-	private final static String CONTAINER_3 = "3";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+        // マネージャを初期化
+        final ExpandAnimatorManager manager = new ExpandAnimatorManager();
 
-		// マネージャを初期化
-		final ExpandAnimatorManager manager = new ExpandAnimatorManager();
+        // コンテナ初期化
+        addExpandAnimator(R.id.trigger1, R.id.container1, CONTAINER_1, manager);
+        initContainer2(manager);
+        addExpandAnimator(R.id.trigger3, R.id.container3, CONTAINER_3, manager);
+        addExpandAnimator(R.id.trigger4, R.id.container4, CONTAINER_4, manager);
+        addExpandAnimator(R.id.trigger5, R.id.container5, CONTAINER_5, manager);
+    }
 
-		// コンテナ初期化
-		addExpandAnimator(R.id.trigger1, R.id.container1, CONTAINER_1, manager);
-		addExpandAnimator(R.id.trigger2, R.id.container2, CONTAINER_2, manager);
-		addExpandAnimator(R.id.trigger3, R.id.container3, CONTAINER_3, manager);
-	}
+    /**
+     * 
+     * @param manager
+     */
+    private void initContainer2(final ExpandAnimatorManager manager) {
+        final Button trigger = (Button) findViewById(R.id.trigger2);
+        trigger.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (manager.get(CONTAINER_2).isExpand()) {
+                    manager.unexpand(CONTAINER_2);
+                } else {
+                    // 他のコンテナを閉じながら自分のコンテナを開きます
+                    manager.exclusiveExpand(CONTAINER_2);
+                }
+            }
+        });
 
-	/**
-	 * ExpandAnimatorをExpandAnimatorManagerに追加します。
-	 * 
-	 * @param triggerId
-	 *            開閉のトリガとなるViewのidを指定して下さい。このViewはクリックが可能である必要があります。
-	 * @param containerId
-	 *            開閉の対象となるViewを指定して下さい。
-	 * @param key
-	 *            ExpandAnimatorManagerにputする為のキーを指定して下さい。
-	 * @param manager
-	 *            ExpandAnimatorを追加するExpandAnimatorManagerを指定して下さい。
-	 */
-	private void addExpandAnimator(int triggerId, int containerId, final String key, final ExpandAnimatorManager manager) {
-		manager.put(key, createAnimator(findViewById(containerId)));
-		findViewById(triggerId).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (manager.get(key).isExpand()) {
-					manager.unexpand(key);
-				} else {
-					manager.expand(key);
+        View v = findViewById(R.id.container2);
+        ExpandAnimator animator = new ExpandAnimator(v, new ExpandAnimator.OnAnimationListener() {
+            @Override
+            public void onExpanded(ExpandAnimator e) {
+                trigger.setText("開いた");
+            }
 
-					// 他のコンテナを閉じながら自分のコンテナを開きます
-					// manager.exclusiveExpand(key);
-				}
-			}
-		});
-	}
+            @Override
+            public void onStartExpand(ExpandAnimator e) {
+                trigger.setText("開いている");
+            }
 
-	/**
-	 * ExpandAnimatorを初期化します。expand/unexpandのリスナやアニメーションの時間、インタポレータをセットする事が出来ます。
-	 * 
-	 * @param v
-	 *            expand/unexpandの対象となるViewを指定して下さい。
-	 * @return 初期化したExpandAnimator
-	 */
-	private ExpandAnimator createAnimator(View v) {
-		ExpandAnimator animator = new ExpandAnimator(v, new ExpandAnimator.OnAnimationListener() {
-			@Override
-			public void onExpanded(ExpandAnimator e) {
-				
-			}
-			@Override
-			public void onStartExpand(ExpandAnimator e) {
-				
-			}
-			@Override
-			public void onStartUnexpand(ExpandAnimator e) {
-				
-			}@Override
-			public void onUnexpanded(ExpandAnimator e) {
-				
-			}
-		});
-		animator.setDuration(700);
-		animator.setInterpolator(new AccelerateDecelerateInterpolator());
-		return animator;
-	}
+            @Override
+            public void onStartUnexpand(ExpandAnimator e) {
+                trigger.setText("閉じている");
+            }
+
+            @Override
+            public void onUnexpanded(ExpandAnimator e) {
+                trigger.setText("閉じた");
+            }
+        });
+        animator.setDuration(1200);
+        animator.setInterpolator(new BounceInterpolator());
+        manager.put(CONTAINER_2, animator);
+        final Random random = new Random(System.currentTimeMillis());
+        final int margin = 155;
+        final LinearLayout container = (LinearLayout)findViewById(R.id.container2_1);
+        findViewById(R.id.add).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = new View(This());
+                int c = Color.rgb(random.nextInt(255-margin)+margin,
+                        random.nextInt(255-margin)+margin,
+                        random.nextInt(255-margin)+margin
+                        );
+                view.setBackgroundColor(c);
+                container.addView(view, 0, new LayoutParams(LayoutParams.FILL_PARENT, 100));
+                manager.adjustSizeImmediately(CONTAINER_2);
+            }
+        });
+        findViewById(R.id.remove).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(container.getChildCount() > 0){
+                    container.removeViewAt(0);
+                    manager.adjustSizeImmediately(CONTAINER_2);
+                }
+            }
+        });
+    }
+
+    /**
+     * ExpandAnimatorをExpandAnimatorManagerに追加します。
+     * 
+     * @param triggerId
+     *            開閉のトリガとなるViewのidを指定して下さい。このViewはクリックが可能である必要があります。
+     * @param containerId
+     *            開閉の対象となるViewを指定して下さい。
+     * @param key
+     *            ExpandAnimatorManagerにputする為のキーを指定して下さい。
+     * @param manager
+     *            ExpandAnimatorを追加するExpandAnimatorManagerを指定して下さい。
+     */
+    private void addExpandAnimator(int triggerId, int containerId, final String key, final ExpandAnimatorManager manager) {
+        manager.put(key, createAnimator(findViewById(containerId)));
+        findViewById(triggerId).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (manager.get(key).isExpand()) {
+                    manager.unexpand(key);
+                } else {
+                    manager.expand(key);
+                    // 他のコンテナを閉じながら自分のコンテナを開きます
+                    // manager.exclusiveExpand(key);
+                }
+            }
+        });
+    }
+
+    /**
+     * ExpandAnimatorを初期化します。expand/unexpandのリスナやアニメーションの時間、インタポレータをセットする事が出来ます。
+     * 
+     * @param v
+     *            expand/unexpandの対象となるViewを指定して下さい。
+     * @return 初期化したExpandAnimator
+     */
+    private ExpandAnimator createAnimator(View v) {
+        ExpandAnimator animator = new ExpandAnimator(v, new ExpandAnimator.OnAnimationListener() {
+            @Override
+            public void onExpanded(ExpandAnimator e) {
+
+            }
+
+            @Override
+            public void onStartExpand(ExpandAnimator e) {
+
+            }
+
+            @Override
+            public void onStartUnexpand(ExpandAnimator e) {
+
+            }
+
+            @Override
+            public void onUnexpanded(ExpandAnimator e) {
+
+            }
+        });
+        animator.setDuration(700);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        return animator;
+    }
 
 }
